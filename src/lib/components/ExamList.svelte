960814c9334e1d5d267b9562cacf98113ee30ae0
Anish Vundela <anishreddyvundela@proton.me>
2025-06-17 16:46:21 +0100

    Create <ExamList /> which creates a list of <ExamCard />s

---
+++ src/lib/components/ExamList.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import ExamCard from "./ExamCard.svelte";
+
+    let examCount = $state({ length: 1 });
+
+    let { class: CLASS = "" } = $props();
+</script>
+
+<div class={CLASS}>
+    {#each examCount as _}
+        <ExamCard class="pb-3" />
+    {/each}
+    <button class="border p-2" onclick={() => examCount.length++}>
+        + Add Exam
+    </button>
+</div>
